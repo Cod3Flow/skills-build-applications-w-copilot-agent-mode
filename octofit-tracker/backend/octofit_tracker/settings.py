1caeeefa945e82c0783032fa56359946f79cb817
Cod3Flow <139870853+Cod3Flow@users.noreply.github.com>
2025-05-08 18:20:55 +0000

    Update allowed hosts and base URL for API in settings and views

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a#elwv#9$vxv%0yfr*wlr^ey_en7sbut1cz14!%#htc(6fs*dy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'orange-waddle-pjr79rvvpvr729xpr-8000.app.github.dev']
 
 
 # Application definition
